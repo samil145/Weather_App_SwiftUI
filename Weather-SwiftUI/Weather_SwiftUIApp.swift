ac05cc31faddfcb9eec2bba5ac6524a23fa7cf00
samil145 <samil.bayramli.sb@gmail.com>
2024-05-13 03:15:09 +0400

    Initial Commit

---
+++ Weather-SwiftUI/Weather_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Weather_SwiftUIApp.swift
+//  Weather-SwiftUI
+//
+//  Created by Shamil Bayramli on 13.05.24.
+//
+
+import SwiftUI
+
+@main
+struct Weather_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
